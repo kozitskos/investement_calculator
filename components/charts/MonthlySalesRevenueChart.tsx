d98fdfb0729fa11fd2ffd2a0327c8c8d73aec37a
kozitskos <kozitskos@gmail.com>
2024-09-09 13:30:35 +0200

    Line added

--- components/charts/MonthlySalesRevenueChart.tsx
+++ components/charts/MonthlySalesRevenueChart.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
+import { Line } from 'react-chartjs-2';
 import { ChartOptions, ChartData } from 'chart.js';
 import 'chart.js/auto';
 
